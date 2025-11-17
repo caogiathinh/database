3b09b7b5abdaf40e8992d6eadc2d8cdb29281c7f
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-11-17 21:47:34 +0700

    Update hands-on

--- hands-on/Part02-SQL/Session01-BasicSelect/05-SELECT-WHERE.sql
+++ hands-on/Part02-SQL/Session01-BasicSelect/05-SELECT-WHERE.sql
@@ -3,38 +3,22 @@
 --------------------------------------------------------------
 -- LÍ THUYẾT 
 -- CÚ PHÁP MỞ RỘNG CỦA LỆNH SELECT
--- Ta muốn săp xép dữ liệu sort theo tiêu chí nào đó, thường là tăng dần - ASCENDING/ASC
---																giảm dần - DESCENDING/DESC
--- mặc định không nói gì cả là sort tăng dần
--- A < B < C
--- 1 < 2 < 3 
--- ta có thể sắp xêp trên nhiều cột, logic này từ từ tính
--- SELECT ... FROM <TÊN-TABLE> ORDER BY <TÊN-CỘT-MUỐN-SORT> <KIỂU SORT>
+-- MỆNH ĐỀ WHERE DÙNG LÀM BỘ LỌC/FILTER/NHẶT RA NHỮNG DỮ LIỆU TA CẦN THEO 1 TIÊU CHÍ NÀO ĐÓ 
+-- Ví dụ: Lọc ra những sinh viên có quê ở Bình Dương
+--		  Lọc ra những sv quê ở Tiền Giang và có điểm trung bình >= 8 
 
+-- CÚ PHÁP DÙNG BỘ LỌC
+-- SELECT * (cột bạn muốn in ra) FROM <TÊN-TABLE> WHERE <ĐIỀU KIỆN LỌC>
+-- * ĐIỀU KIỆN LỌC: TÌM TỪNG DÒNG, VỚI CÁI CỘT CÓ GIÁ TRỊ CẦN LỌC
+--					 LỌC THEO TÊN CỘT VỚI VALUE THẾ NÀO, LẤY TÊN CỘT, XEM VALUE TRONG CELL
+--					 CÓ THỎA ĐIỀU KIỆN LỌC HAY KHÔNG ? 
+-- ĐỂ VIẾT ĐƯỢC ĐIỀU KIỆN LỌC TA CẦN:
+-- > tên cột
+-- > value của cột (cell)
+-- > toán tử (operator) >= > < <= = (1 dấu =, không giống như C hay Java), != (hoặc <> cùng ý nghĩa)
+-- > nhiều điều kiện lọc đi kèm nhau dùng thêm toán tử logic operators, AND, OR, NOT
+-- Ví dụ: WHERE City = N'Binh Dương'
+--        WHERE City = N'Tiền Giang' AND Gpa >= 8
 --------------------------------------------------------------
 -- THỰC HÀNH
--- 1. In ra  danh sách nhân viên
-SELECT * FROM Employees
-
--- 2. In ra danh sách nhân viên tăng dần theo năm sinh
-SELECT * FROM Employees ORDER BY BirthDate ASC
-SELECT * FROM Employees ORDER BY BirthDate ASC --mặc định là tăng dần
-
--- 3. In ra ds nhân viên GIẢM DẦN theo năm sinh
-SELECT * FROM Employees ORDER BY BirthDate DESC
-
--- 4. Tính tiền chi tiết mua hàng
-SELECT * FROM [Order Details]
-SELECT *, Quantity * UnitPrice * (1 - Discount) AS SubTotal FROM [Order Details]
-
--- 5. Tính tiền chi tiết mua hàng, giảm dần theo số tiền
-SELECT *, Quantity * UnitPrice * (1 - Discount) AS SubTotal FROM [Order Details] 
-ORDER BY SubTotal DESC
-
--- 6. In ra danh sách nhân viên giảm dần theo tuổi
-SELECT *, YEAR(GETDATE()) - YEAR(BirthDate) AS Age FROM Employees  
-ORDER BY Age DESC
-
-SELECT EmployeeID, FirstName, BirthDate, YEAR(GETDATE()) - YEAR(BirthDate) AS [Age]
-FROM Employees
-ORDER BY Age DESC
+-- 1. 
