79b53317d306e8c339ee296438c230ffe588d953
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-11-19 08:13:06 +0700

    Update hands-on BasicSelect where

--- hands-on/Part02-SQL/Session01-BasicSelect/05-SELECT-WHERE.sql
+++ hands-on/Part02-SQL/Session01-BasicSelect/05-SELECT-WHERE.sql
@@ -31,6 +31,30 @@
 SELECT * FROM Customers --92
 
 -- 2. In ra danh sách khách hàng đến từ Ý
-SELECT * FROM Customers WHERE Country = 'Italy';
+SELECT * FROM Customers WHERE Country = 'Italy'; --3/92
 
--- 3. 
+-- 3. In ra danh sách khách hàng đến từ Mĩ
+SELECT * FROM Customers WHERE Country = 'USA' --13 
+
+-- 4. In ra những khách hàng đến từ Mĩ và Ý
+-- đời thường có thể nói: những k/h đến từ Ý và Mĩ, Ý hoặc Mĩ 
+
+-- SELECT * FROM Customers WHERE Country = 'USA' AND 'Italy'-- thiếu toán tử so sánh
+
+SELECT * FROM Customers WHERE Country = 'USA' AND Country = 'Italy'    -- 0
+SELECT * FROM Customers WHERE Country = 'USA' OR Country = 'Italy'     --16, hiểu logic hơn là hiểu tiếng Việt
+
+--sort theo Ý, Mĩ, đê gom cùng cụm cho dễ theo dõi
+
+SELECT * FROM Customers WHERE Country = 'USA' OR Country = 'Italy' ORDER BY Country
+
+-- 5. In ra k/h đến từ thủ đô nước Đức
+SELECT * FROM Customers WHERE Country = 'Germany' AND City = 'Berlin' -- 1
+
+-- 6. In ra các thông tin của nhân viên
+SELECT * FROM Employees
+
+-- 7. In ra thông tin của nhân viên từ năm 1950 trở lại gần đây/đổ lại
+SELECT * FROM Employees WHERE YEAR(BirthDate) >= 1960
+
+-- 8. 
