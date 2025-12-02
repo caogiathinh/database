6d7b1eea00635ecad087b9399367034a8d3a139b
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-12-02 22:23:56 +0700

    Add folder Session06-WindowFunction

---
+++ hands-on/Part02-SQL/Session06-WindowFunction/windowFunction.sql
@@ -0,0 +1,29 @@
+﻿--Find the tota Sale Across all orders
+SELECT 
+	SUM(Sales) TotalSales
+FROM Sales.Orders
+
+--Find the total sales for each product
+SELECT 
+	ProductID,
+	SUM(Sales) TotalSales
+FROM Sales.Orders
+GROUP BY ProductID
+
+----Find the total sales for each product
+-- provides detail OrderId, OrderDate
+SELECT 
+	OrderID,
+	OrderDate,
+	ProductID,
+	SUM(Sales) TotalSales
+FROM Sales.Orders
+GROUP BY ProductID, OrderID, OrderDate
+
+--window function trả về kết quả sau mỗi dòng
+SELECT 
+	OrderID,
+	OrderDate,
+	ProductID, 
+	SUM(Sales) OVER(PARTITION BY ProductID) AS TotalSalesByProducts
+FROM Sales.Orders
