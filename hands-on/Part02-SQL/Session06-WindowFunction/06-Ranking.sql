d8be0f855d0dfcfdecb5f2a9e0ac5c8baea3e227
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-12-04 13:57:17 +0700

    Update folder Session06-WindowFunction

--- hands-on/Part02-SQL/Session06-WindowFunction/06-Ranking.sql
+++ hands-on/Part02-SQL/Session06-WindowFunction/06-Ranking.sql
@@ -52,3 +52,46 @@ FROM(
 )t WHERE rn = 1
 -- partition by orderId, tức là partition theo khóa chính
 -- sau đó, dùng hàm row_number() để đếm kết quả thằng nào >1 là bị trùng
+
+-- NTILE
+SELECT 
+	OrderID,
+	Sales,
+	NTILE(1) OVER(ORDER BY Sales DESC) OneBucket,
+	NTILE(2) OVER(ORDER BY Sales DESC) TwoBucket,
+	NTILE(3) OVER(ORDER BY Sales DESC) ThreeBucket,
+	NTILE(4) OVER(ORDER BY Sales DESC) FourBucket
+FROM Sales.Orders
+
+-- Segment all orders into 3 categories: high, medium and low sales
+SELECT
+	*, 
+	CASE WHEN Buckets = 1 THEN 'High'
+		 WHEN Buckets = 2 THEN 'Medium'
+		 WHEN Buckets = 3 THEN 'Low'
+	END SaleSegementations
+FROM(
+SELECT
+	OrderID, 
+	Sales, 
+	NTILE(3) OVER(ORDER BY Sales DESC) Buckets
+FROM Sales.Orders
+)t 
+
+-- In order to export the data, divide the orders into 2 groups
+SELECT
+	NTILE(2) OVER(ORDER BY OrderID) Buckets, 
+	*
+FROM Sales.Orders
+
+-- Find the products that fall within the highest 40% of the prices
+SELECT 
+	*, 
+	CONCAT(diskRank * 100, '%') DistRankPerc
+FROM (
+SELECT
+	ProductID, 
+	Price, 
+	CUME_DIST() OVER(ORDER BY Price DESC) diskRank
+FROM Sales.Products
+) t WHERE diskRank <= 0.4
---
+++ hands-on/Part02-SQL/Session06-WindowFunction/07-value.sql
@@ -0,0 +1,9 @@
+﻿-- Analyze the month-over-month (MoM) performance by finding
+-- the percentage change in sales between the current and previous months
+
+SELECT
+	MONTH(OrderDate) OrderMonth, 
+	SUM(Sales) CurrentMonthSales,
+    LAG(SUM(Sales)) OVER(ORDER BY MONTH(OrderDate)) PreviousMonthSales
+FROM Sales.Orders
+GROUP BY MONTH(OrderDate)
