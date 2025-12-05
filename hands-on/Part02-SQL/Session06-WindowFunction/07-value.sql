e55b31c92490411b1739ebcf55ebc1480d69497f
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-12-05 23:01:34 +0700

    Update folder Session06-WindowFunction

--- hands-on/Part02-SQL/Session06-WindowFunction/07-value.sql
+++ hands-on/Part02-SQL/Session06-WindowFunction/07-value.sql
@@ -1,9 +1,14 @@
 ﻿-- Analyze the month-over-month (MoM) performance by finding
 -- the percentage change in sales between the current and previous months
-
+SELECT 
+	*, 
+	CurrentMonthSales - PreviousMonthSales AS Mom_Change,
+	ROUND(CAST((CurrentMonthSales - PreviousMonthSales) AS FLOAT) / PreviousMonthSales * 100, 1) AS Mom_Perc
+FROM ( 
 SELECT
 	MONTH(OrderDate) OrderMonth, 
 	SUM(Sales) CurrentMonthSales,
     LAG(SUM(Sales)) OVER(ORDER BY MONTH(OrderDate)) PreviousMonthSales
 FROM Sales.Orders
 GROUP BY MONTH(OrderDate)
+) t 
