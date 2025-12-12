c8e18a8dfee8c97fd22ae08a389683a3b833b8f0
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-12-12 15:05:33 +0700

    Update folder Session04-Join

--- hands-on/Part02-SQL/Session04-JOIN/00-StudentManagement.sql
+++ hands-on/Part02-SQL/Session04-JOIN/00-StudentManagement.sql
@@ -104,8 +104,117 @@ FROM Student s RIGHT JOIN Major m
 
 SELECT s.*, m.* 
 FROM Major m LEFT JOIN Student s 
-	ON s.MajorID = m.MajorID
+	ON s.MajorID = m.MajorID --10 dòng, korean fa null
 
 -- thứ tự table đặt trong mệnh đề FROM để OUTER JOIN cực kì quan trọng
 -- lấy bên nào làm gốc 
 
+SELECT s.*, m.* 
+FROM Major m RIGHT JOIN Student s 
+	ON s.MajorID = m.MajorID --9 dòng, korean biến mất
+
+-- 4. Có bao nhiêu chuyên ngành ?? 
+SELECT COUNT(*) AS [No Major]
+FROM Major
+
+SELECT COUNT(MajorID) AS [No Major]
+FROM Major
+
+-- 5. Mỗi chuyên ngành có bao nhiêu sinh viên
+-- output 0: số lượng sv đang theo học của từng chuyên ngành
+-- output 1: mã cn | số sinh viên đang theo học
+-- phân tích: đếm sinh viên, bao nhiêu sinh viên, đếm sv sure
+-- gặp thêm từ mỗi 
+-- mỗi chuyên ngành có một con số đếm, đếm theo chuyên ngành, nhóm chuyên ngành mà đếm
+SELECT MajorID, COUNT(*) AS [No Student]
+FROM Student
+GROUP BY MajorID 
+
+SELECT MajorID, COUNT(MajorID) AS [No Student]
+FROM Student
+GROUP BY MajorID
+
+-- 6. Chuyên ngành nào có từ 3 sinh viên trở lên ? 
+-- Phân tích: chia chặng ròi
+--			  đầu tiên đếm chuyên ngành đã, quét qua bảng 1 lần về đếm ra sv
+--			  đếm xong, dợt lại kết quả, lọc thêm cái từ 3 sv trở lên
+--			  phải đếm xong từng ngành sau đó tính tiếp
+SELECT MajorID, COUNT(*) AS [No Student]
+FROM Student 
+GROUP BY MajorID
+HAVING COUNT(*) >= 3
+
+-- 7. Chuyên ngành nào có ít sính viên nhất ? 
+-- cách 1: dùng toán tử all
+SELECT MajorID, COUNT(*) AS [No Student]
+FROM Student
+GROUP BY MajorID
+HAVING COUNT(*) <= ALL(
+						SELECT COUNT(*) AS [No Student]
+						FROM Student
+						GROUP BY MajorID
+						)
+-- cách 2: so sánh với max
+SELECT MajorID, COUNT(*) AS [No Student]
+FROM Student
+GROUP BY MajorID
+HAVING COUNT(*) = 
+				(
+					SELECT MIN([No Student])
+					FROM (
+						SELECT COUNT(*) AS [No Student]
+					    FROM Student
+				        GROUP BY MajorID) AS MinEle
+				)
+
+--coi bảng thống kê sinh viên theo chuyên ngành tìm max sau đó so sánh với max 
+
+-- 8. Đếm số sinh viên của chuyên ngành SE
+SELECT MajorID, COUNT(*) AS [No student]
+FROM Student 
+WHERE MajorID = 'SE'
+GROUP BY MajorID --câu này nhanh hơn
+
+-- cách group by sau đó lọc se ra sẽ có hiệu suất chạy chậm hơn
+SELECT MajorID, COUNT(*) AS [No student]
+FROM Student 
+GROUP BY MajorID
+HAVING MajorID = 'SE'
+
+-- 9. Đếm số sinh viên của mỗi chuyên ngành 
+-- output: mã chuyên ngành, tên chuyên ngàng, số lượng sinh viên
+-- phân tích; đáp án cần có info của 2 table
+--			  đếm trên 2 table 
+--			  đếm trong Major hong có info sv
+--			  đếm trong sv chỉ có được mã cn
+--		      muốn có mã cn, tên cn, số lượng sv -> JOIN 2 BẢNG RỒI MỚI ĐẾM
+
+SELECT s.FirstName, s.LastName,  m.MajorID, m.MajorName
+FROM Student s INNER JOIN Major m
+	ON s.MajorID = m.MajorID
+
+SELECT m.MajorID, m.MajorName, COUNT(*) AS [No Student]
+FROM Student s INNER JOIN Major m
+	ON s.MajorID = m.MajorID
+GROUP BY m.MajorID, m.MajorName
+
+-- 10. 
+-- thế còn chuyên ngành Korean thì sao
+-- bị miss mất chuyên ngành hàn quốc
+SELECT s.FirstName, s.LastName,  m.MajorID, m.MajorName
+FROM Student s RIGHT JOIN Major m
+	ON s.MajorID = m.MajorID
+	
+SELECT m.MajorID, m.MajorName, COUNT(*) AS [No Student]
+FROM Student s RIGHT JOIN Major m
+	ON s.MajorID = m.MajorID
+GROUP BY m.MajorID, m.MajorName --vì có một dòng Korean FA, NULL VỀ SV
+								--COUNT(1) CÓ 1 DÒNG HÀN QUỐC FA, HQ CÓ 1 SV SAI
+
+SELECT m.MajorID, m.MajorName, COUNT(s.StudentID) AS [No Student]
+FROM Student s RIGHT JOIN Major m
+	ON s.MajorID = m.MajorID
+GROUP BY m.MajorID, m.MajorName
+
+-- đếm trên mã số sinh viên thay vì đếm trên dòng sẽ bị sai
+-- dashboard màn hình thống kế của admin tuyển sinh
