541af493aed94a6d8efc978eaefe83d55d227997
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-12-13 21:42:19 +0700

    Update folder Session04-Join

--- hands-on/Part02-SQL/Session04-JOIN/00-ClubManagement.sql
+++ hands-on/Part02-SQL/Session04-JOIN/00-ClubManagement.sql
@@ -64,10 +64,155 @@ INSERT INTO Registration(StudentID, ClubID) VALUES('SE2', 'CSG')
 
 INSERT INTO Registration(StudentID, ClubID) VALUES('SE3', 'SiTi')
 
-SELECT * FROM Registration
-
 SELECT * FROM Student
 SELECT * FROM Club
 SELECT * FROM Registration
 
+-- bài toán trên mối quan hệ n - n
+SELECT * FROM Student
 
+-- THỰC HÀNH
+-- 1. Liệt kê thông sinh viên đang theo học 
+SELECT * FROM Student
+
+-- 2. Liệt kê thông tin sinh viên đang theo học kèm theo clb bạn í tham gia
+-- Output1: Mã sv, tên sv, mã clb
+-- Output2: Mã sv, tên sv, mã clb, tên clb
+SELECT * FROM Student
+SELECT * FROM Registration
+
+-- cần hiểu kiến trúc table đế có thể join đúng
+SELECT * --ghép 2 table sát nhau, nhiều cột
+FROM Student s JOIN Registration r
+	ON s.StudentID = r.StudentID
+
+SELECT s.StudentID, s.FirstName + ' '  + s.LastName AS [Full Name], r.ClubID--ghép 2 table sát nhau, nhiều cột
+FROM Student s JOIN Registration r
+	ON s.StudentID = r.StudentID --rút bớt cột
+-- thiếu mất sv 4, 5 vì INNER JOIN
+
+SELECT s.StudentID, s.FirstName + ' '  + s.LastName AS [Full Name], r.ClubID--ghép 2 table sát nhau, nhiều cột
+FROM Student s LEFT JOIN Registration r
+	ON s.StudentID = r.StudentID
+
+-- 3. In ra thông tin tham gia clb của các sv
+-- output: mã sv, tên sv, mã clb, tên clb, join date
+SELECT *
+FROM Student s LEFT JOIN Registration r 
+	ON s.StudentID = r.StudentID 
+	JOIN Club c ON r.ClubID = c.ClubID
+
+SELECT s.StudentID, s.FirstName, c.ClubID, c.ClubName, r.JoinedDate
+FROM Student s JOIN Registration r 
+	ON s.StudentID = r.StudentID 
+	JOIN Club c ON r.ClubID = c.ClubID
+--vãn đề lớn: mất nó clb FCode FEV, và sinh viên SE4, SE5
+
+SELECT s.StudentID, s.FirstName, c.ClubID, c.ClubName, r.JoinedDate
+FROM Student s, Registration r, Club c
+WHERE s.StudentID = r.StudentID AND r.ClubID = c.ClubID
+--với cách này ko thể lấy được phần riêng của từng bảng, cách viết thực dụng 
+--khi lấy được phần hụt, vì nó chỉ đi tìm đám bằng nhau common field
+--ghép và in ra, thằng nào bị hụt, kệ. 
+
+-- JOIN SẼ GIÚP, VÌ NÓ NHÌN LUÔN PHẦN CHUNG VÀ PHẦN BỊ HỤT
+SELECT s.StudentID, s.FirstName, c.ClubID, c.ClubName, r.JoinedDate
+FROM Student s FULL JOIN Registration r 
+	ON s.StudentID = r.StudentID 
+	FULL JOIN Club c ON r.ClubID = c.ClubID    --11
+
+-- Bài tập
+-- 1. Đếm số clb mà mỗi sv đã tham gia
+-- Output: mã sv, tên sv, số clb-tham-gia
+SELECT s.StudentID, s.FirstName, COUNT(r.StudentID) AS [No clbs]
+FROM Student s LEFT JOIN Registration r
+	ON s.StudentID = r.StudentID
+GROUP BY s.StudentID, s.FirstName
+
+-- 2. Sinh viên SE1 tham gia mấy clb
+-- Output: mã sv, tên sv, số clb-tham-gia
+SELECT s.StudentID, s.FirstName, COUNT(r.StudentID) AS [No clbs]
+FROM Student s LEFT JOIN Registration r
+	ON s.StudentID = r.StudentID
+WHERE s.StudentID = 'SE1'
+GROUP BY s.StudentID, s.FirstName
+
+-- 3. Sinh viên nào tham gia nhiều clb nhất ?? -- SE1, SE2
+SELECT s.StudentID, s.FirstName, COUNT(r.StudentID) AS [No clbs]
+FROM Student s LEFT JOIN Registration r
+	ON s.StudentID = r.StudentID
+GROUP BY s.StudentID, s.FirstName
+HAVING COUNT(r.StudentID) = 
+							(
+								SELECT MAX([No clbs])
+								FROM(
+								SELECT COUNT(r.StudentID) AS [No clbs]
+								FROM Student s LEFT JOIN Registration r
+									ON s.StudentID = r.StudentID
+								GROUP BY s.StudentID, s.FirstName) GoCLB
+							)
+
+SELECT s.StudentID, s.FirstName, COUNT(r.StudentID) AS [No clbs]
+FROM Student s LEFT JOIN Registration r
+	ON s.StudentID = r.StudentID
+GROUP BY s.StudentID, s.FirstName
+HAVING COUNT(r.StudentID) >= ALL 
+							(
+								SELECT COUNT(r.StudentID) AS [No clbs]
+								FROM Student s LEFT JOIN Registration r
+									ON s.StudentID = r.StudentID
+								GROUP BY s.StudentID, s.FirstName
+							)
+-- 4. CLB Cộng động Sinh viên Tình nguyện có những sv nào tham gia (gián tiếp)
+-- không dùng sub cũng okie. nhớ là tui không có hỏi SiTi à nhen
+SELECT r.StudentID, c.ClubName
+FROM Club c JOIN Registration r
+	ON c.ClubID = r.ClubID
+WHERE c.ClubName = N'Cộng đồng Sinh viên Tình nguyện'
+
+-- 5. Mỗi clb có bao nhiêu thành viên
+-- Output: mã clb, tên clb, số thành viên
+SELECT c.ClubID, c.ClubName, COUNT(r.ClubID) AS [No Students]
+FROM Club c LEFT JOIN Registration r
+	ON c.ClubID = r.ClubID
+GROUP BY c.ClubID, c.ClubName
+
+-- 6. Clb nào đông member nhất
+-- Output: mã clb, tên clb, số thành viên
+SELECT c.ClubID, c.ClubName, COUNT(r.ClubID) AS [No Students]
+FROM Club c LEFT JOIN Registration r
+	ON c.ClubID = r.ClubID
+GROUP BY c.ClubID, c.ClubName
+HAVING COUNT(r.ClubID) >= ALL
+							(
+								SELECT COUNT(r.ClubID) AS [No Students]
+								FROM Club c LEFT JOIN Registration r
+									ON c.ClubID = r.ClubID
+								GROUP BY c.ClubID, c.ClubName
+							)
+
+SELECT c.ClubID, c.ClubName, COUNT(r.ClubID) AS [No Students]
+FROM Club c LEFT JOIN Registration r
+	ON c.ClubID = r.ClubID
+GROUP BY c.ClubID, c.ClubName
+HAVING COUNT(r.ClubID) =
+						(
+							SELECT MAX([No Students])
+							FROM(
+							SELECT COUNT(r.ClubID) AS [No Students]
+							FROM Club c LEFT JOIN Registration r
+								ON c.ClubID = r.ClubID
+							GROUP BY c.ClubID, c.ClubName) ClubGroup
+						)
+
+-- 7. CLB SiTi và CSG có bao nhiêu member. Đếm riêng từng clb
+-- Output: mã clb, tên clb, số thành viên (2 dòng)
+SELECT c.ClubID, c.ClubName, COUNT(r.ClubID) AS [No Student]
+FROM Club c JOIN Registration r
+	ON c.ClubID = r.ClubID
+WHERE C.ClubID IN('CSG', 'SiTi')
+GROUP BY c.ClubID, c.ClubName
+
+-- 8. Có tổng cộng bao nhiêu lượt sinh viên tham gia clb
+SELECT COUNT(*) AS [No Student]
+FROM Registration
